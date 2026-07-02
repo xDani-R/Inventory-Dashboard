600b01e9de3442398e395b2c45b018bd1fa87cd7
root <root@ip-10-0-171-56.ec2.internal>
2026-07-02 03:47:05 +0000

    Agrega manifiestos Kubernetes y despliegue EKS

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  host: "127.0.0.1",
-  port: 3306,
-  user: "root",
-  password: "root123",
-  database: "inventory_dashboard",
+  host: process.env.DB_HOST || "127.0.0.1",
+  port: process.env.DB_PORT || 3306,
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "root123",
+  database: process.env.DB_NAME || "inventory_dashboard",
   waitForConnections: true,
   connectionLimit: 10,
 });
